692af52ee87c60bea922cc9c1a4095617ff614ec
Luiz Lobo <luizfpll@gmail.com>
2023-05-24 08:14:50 +0200

    adding config loader

---
+++ config/load.py
@@ -0,0 +1,35 @@
+import pdb
+import yaml
+from config_models import Auth, GlobalSettings, Endpoint, Config
+import sys
+from pydantic import FilePath
+
+
+def load_config(yaml_path: FilePath):
+
+    # Load the YAML configuration file
+    with open(yaml_path, 'r') as file:
+        raw_config = yaml.safe_load(file)
+
+    # Parse it into the dataclasses
+    auth = Auth(**raw_config['AUTH'])
+    global_settings = GlobalSettings(**raw_config['GLOBAL_SETTINGS'])
+
+    # get's the endpoints and their children
+    endpoints = {name: Endpoint(**config)
+                 for name, config in raw_config['ENDPOINTS'].items()}
+
+    config = Config(AUTH=auth,
+                    GLOBAL_SETTINGS=global_settings,
+                    ENDPOINTS=endpoints)
+    
+    for endpoint in config.ENDPOINTS.values():
+        endpoint.propagate_default_config_to_child()
+
+    return config
+
+
+config = load_config(sys.argv[1])
+
+
+
---
+++ config/models.py
@@ -0,0 +1,58 @@
+from typing import List, Dict,  Optional
+from pydantic import BaseModel, Field
+
+class Auth(BaseModel):
+    type: str = Field(...)
+    token_name: str = Field(...)
+
+class GlobalSettings(BaseModel):
+    rate_limit: int = Field(...)
+    retry_attempts: int = Field(...)
+    timeout_seconds: int = Field(...)
+
+class Mode(BaseModel):
+    type: str = Field(...)
+    incremental_column: str = Field(...)
+    methodology: str = Field(...)
+    pagination: Optional[Dict[str, str]]
+
+class ErrorHandle(BaseModel):
+    ignore: List[int] = Field(...)
+    warning:  List[int] = Field(...)
+
+class BaseEndpoint(BaseModel):
+    HEADERS: Optional[Dict[str, str]] = None
+    URL: Optional[str] = None
+    HTTP_METHOD: Optional[str] = None
+    MODE: Optional[Mode] = None
+    CHILDREN_ARGUMENTS: Optional[Dict[str, str]] = None
+    INSERT_CHILDREN: Optional[Dict[str, str]] = None
+    CHILDREN: Optional[Dict[str, 'BaseEndpoint']] = None 
+    KEY_PROPERTIES: Optional[List[str]] = None  
+    ERROR_HANDLE: ErrorHandle
+
+
+class Endpoint(BaseEndpoint):
+    INITIAL_ARGUMENTS: Optional[Dict[str, List[str]]] = None
+    
+    
+    def propagate_default_config_to_child(self, endpoint: BaseEndpoint = None):
+        propagate = ['HEADERS','HTTP_METHOD','MODE','INSERT_CHILDREN']
+        # Propagates default parent endpoint configs
+        if endpoint is None:
+            endpoint = self
+        if endpoint.CHILDREN is not None:
+            for _,child in endpoint.CHILDREN.items():
+                for key in propagate:
+                    if getattr(child,key) is None:
+                        father_val = getattr(endpoint,key)
+                        setattr(child,key,father_val)
+                self.propagate_default_config_to_child(child)
+        return 
+
+class Config(BaseModel):
+    AUTH: Auth
+    GLOBAL_SETTINGS: GlobalSettings
+    ENDPOINTS: Dict[str, Endpoint]
+
+BaseEndpoint.update_forward_refs()
